4e85e781464f60acba77121eee5598b4cd240a3e
jmb488 <jeff.barker@kellogg.northwestern.edu>
2020-02-18 02:23:14 +0000

    Dice stuff

--- dice.rb
+++ dice.rb
@@ -4,7 +4,14 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  "<h1>Hello world</h1>"
+    @die_1 = rand(1..6)
+    @die_2 = rand(1..6)
+    view "dice"
+    #"<p>First die: #{die_1}</p>
+    #<p>Second die: #{die_2}</p>
+    #<p>Total roll: #{die_1 + die_2}</p>"
+ 
+  
 end
 
 #get "/tacos" do
